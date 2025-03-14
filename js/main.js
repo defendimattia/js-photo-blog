e7cedf5e79eef3b3d637f1a31e6a0f08c22ac301
defendimattia <defendi.mattia92@gmail.com>
2025-03-14 22:03:09 +0100

    refactor: improve code readability and variable naming

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const source = "https://lanciweb.github.io/demo/api/pictures/"
 
-const cardsImgElement = document.querySelectorAll('.card-img');
+const cardsImgElements = document.querySelectorAll('.card-img');
 
 
 
@@ -9,25 +9,25 @@ axios.get(source)
     .then(response => {
 
         // array che contiene gli oggetti presi dall'API
-        const objArray = response.data
+        const pictures = response.data
 
         // iterazione su ogni oggetto dell'array
-        objArray.forEach(obj => {
+        pictures.forEach(picture => {
 
             // definisco una costante che contiene un elemento img da inserire nel DOM
             const imgElement = document.createElement('img')
 
             // inserisco l'url corrispondente dell'oggetto nel src dell'elemento immagine appena creato
-            imgElement.src = obj.url
+            imgElement.src = picture.url
 
             // calcolo l'indice dell'elemento in cui inserire l'immagine
-            const indexCardsImgElement = obj.id - 1
+            const imageIndex = picture.id - 1
 
-            // svuoto il contenuto precedente dell'elemento .card-img selezionato con indexCardsImgElement
-            cardsImgElement[indexCardsImgElement].innerHTML = ""
+            // svuoto il contenuto precedente dell'elemento .card-img selezionato con imageIndex
+            cardsImgElements[imageIndex].innerHTML = ""
 
             // Aggiungo l'elemento <img> appena creato come figlio dell'elemento .card-img corrispondente
-            cardsImgElement[indexCardsImgElement].appendChild(imgElement)
+            cardsImgElements[imageIndex].appendChild(imgElement)
         });
     })
 
